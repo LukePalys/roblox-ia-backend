519830cf4d88bb09730d4e9c501444552bdbf673
LukePalys <lucas.gabriel.18273@gmail.com>
2025-08-14 19:35:43 -0300

    Dando reset nessa bagaça

--- server.py
+++ server.py
@@ -39,4 +39,4 @@ def root():
 if _name_ == "_main_":
     import uvicorn
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
